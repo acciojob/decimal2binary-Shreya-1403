0390286d1ce5d15b942c3e5a861cc51b1806498f
Shreya-1403 <67495439+Shreya-1403@users.noreply.github.com>
2023-06-17 18:18:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let i=0;
+	let sum=0;
+  while(target!=0){
+	  let rem=target%10;
+	  sum=sum*10+rem;
+	  target=target/10;
+  }
+	return sum;
 }
 
 module.exports = threeSum;
