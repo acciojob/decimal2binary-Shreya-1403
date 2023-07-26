2b1f71dfa896e67c8dbd1b7d008766152265f7c2
Shreya-1403 <67495439+Shreya-1403@users.noreply.github.com>
2023-07-26 20:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function decimalToBinary(num) {
 	let arr=[];
 	let r;
 	let i=0;
-	while(num!=0){
+	while(num!==0){
 		r=num%2;//0 1 0 1
-		arr[i]+=r;//0101 
-		num=num/2;//0
+		arr[i]=r;//0101 
+		num=Math.floor(num/2);//0
+		console.log(num);
 		i++;
 	}
 	arr.reverse();
